6cf536a3d7d05852f78da791c207147a5ca314df
Ethan Tarr <ebt0640@yahoo.com>
2016-11-08 18:14:41 -0800

    Exercise 4
    finished exercise 4

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -2,19 +2,19 @@
 
 # Fix the changes to these three code snippets from the M9 exercise
 # Install the nycflights13 package and read it in. Require the dplyr package.
-install.packages("nycflights13")
-library(nycflights)
+#install.packages("nycflights13")
+library(nycflights13)
 library("dplyr")
 
 # In which month was the average departure delay the greatest?
 dep.delay.by.month <- flights %>% 
-  group_by(months)  
+  group_by(month) %>% 
   summarise(delay = sum(dep_delay, rm=TRUE))
 
 # In which airport were the average arrival delays the highest?
 arr.delay.by.month <- flights %>%  
-  group.by(dast) %>% 
-  summarise(delay == mean(dep_delay %>% 
+  group_by(dest) %>% 
+  summarise(delay == mean(dep_delay)) %>% 
   select(arrange(delay))
   
 # Each team member should write a function that takes in a vector of movie titles and assigns a random rating
